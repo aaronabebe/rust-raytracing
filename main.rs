c82dcaa9d50be85cdab2f934a30d6601f58f2180
Aaron Abebe <aaron.abebe@gmail.com>
2022-04-12 16:04:00 +0200

    cleaned up code

--- main.rs
+++ main.rs
@@ -2,6 +2,21 @@ use gtk::prelude::*;
 use gtk::{Application, DrawingArea};
 
 
+fn draw_gradient(cr: &cairo::Context, width: u32, height: u32) {
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = (i as f64) / ((width - 1) as f64);
+            let g = (j as f64) / ((height - 1) as f64);
+            let b = 0.25;
+
+            cr.rectangle(i as f64, j as f64, 1.0, 1.0);
+            cr.set_source_rgb(r, g, b);
+            cr.fill().expect("Fill did not work!");
+        }
+    }
+}
+
+
 fn build_ui(app: &Application) {
     let window = gtk::ApplicationWindow::new(app);
     let drawing_area = Box::new(DrawingArea::new)();
@@ -9,26 +24,12 @@ fn build_ui(app: &Application) {
     let height = 500;
 
     drawing_area.set_draw_func(move |_, cr, _, _| {
-        for j in (0..height).rev() {
-            for i in 0..width {
-                let r = (i as f64) / ((width - 1) as f64);
-                let g = (j as f64) / ((height - 1) as f64);
-                let b = 0.25;
-
-                //println!("[{}|{}]: [{}, {}, {}]", i, j, r, g, b);
-                cr.rectangle(i as f64, j as f64, 1.0, 1.0);
-                cr.set_source_rgb(r, g, b);
-                cr.fill()
-                    .expect("Fill did not work!");
-            }
-        }
-        //cr.set_source_rgb(0.5, 0.5, 1.0);
+        draw_gradient(cr, width, height);
     });
     window.set_default_size(width as i32, height as i32);
     window.set_child(Some(&drawing_area));
     window.present();
 }
-
 
 fn main() {
     let app = Application::builder()
@@ -38,28 +39,3 @@ fn main() {
     app.run();
 }
 
-
-/*
-fn main() -> () {
-    const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
-
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
-
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0.25;
-
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
-
-            println!("{} {} {}", ir, ig, ib);
-        }
-    }
-}
-*/
