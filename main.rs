9048945bc4796a7c3b38fb93f29094c8784df955
Aaron Abebe <aaron.abebe@gmail.com>
2022-04-13 22:18:14 +0200

    added sphere with simple shading

--- main.rs
+++ main.rs
@@ -8,9 +8,29 @@ use vec::{Vec3, Color, Point3};
 use ray::Ray;
 
 fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalized();
+        return 0.5 * Color::new(n.x() + 0.8, n.y() + 0.8, n.z() + 0.8);
+        //return Color::new(0.2, 0.9, 0.5);
+    }
+
     let unit_direction = r.direction().normalized();
     let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Color::new(1.0, 0.5, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.0, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 
@@ -26,11 +46,13 @@ fn render(cr: &cairo::Context, width: u64, height: u64, aspect_ratio: f64) {
     let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0
                           - Vec3::new(0.0, 0.0, focal_length);
 
-    for j in (0..height).rev() {
+    for j in 0..height {
         eprint!("\rScanlines remaining: {:3}", height - j - 1);
         stderr().flush().unwrap();
 
-        for i in 0..width {
+        // +1 to get rid of weird white line on the edge
+        // dont really know why this appears
+        for i in 0..(width + 1) {
             let u = (i as f64) / ((width - 1) as f64);
             let v = (j as f64) / ((height - 1) as f64);
 
@@ -40,7 +62,7 @@ fn render(cr: &cairo::Context, width: u64, height: u64, aspect_ratio: f64) {
             );
             let px = ray_color(&r);
 
-            cr.rectangle(i as f64, j as f64, 1.0, 1.0);
+            cr.rectangle((width - i) as f64, (height - j) as f64, 1.0, 1.0);
             cr.set_source_rgb(px.x(), px.y(), px.z());
             cr.fill().expect("Fill did not work!");
         }
